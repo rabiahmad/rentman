5ac1f864699ccd8ada901ce426b0e139b7d79d8e
Rabi Ahmad <rabihassanahmad@gmail.com>
2023-07-10 21:17:06 +0100

    updated LoadingSpinner component

--- frontend/src/components/LoadingSpinner.js
+++ frontend/src/components/LoadingSpinner.js
@@ -1,7 +1,11 @@
 import Spinner from "react-bootstrap/Spinner";
 
 const LoadingSpinner = () => {
-  return <Spinner animation="border" />;
+  return (
+    <>
+      <Spinner animation="border" variant="primary" />{" "}
+    </>
+  );
 };
 
 export default LoadingSpinner;
